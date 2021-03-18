1578098bbf8d927235fa727fe7f83ce2be3796c1
issei morita <isseimorita1@gmail.com>
2021-03-18 22:27:29 +0900

    split files by scene

---
+++ main-scene.js
@@ -0,0 +1,13 @@
+phina.define('MainScene', {
+  superClass: 'CanvasScene',
+  init: function() {
+    this.superInit()
+    // 背景色を指定
+    this.backgroundColor = '#444'
+    // ラベルを生成
+    this.label = Label('Hello, phina.js!').addChildTo(this)
+    this.label.x = this.gridX.center() // x 座標
+    this.label.y = this.gridY.center() // y 座標
+    this.label.fill = 'white' // 塗りつぶし色
+  }
+})
