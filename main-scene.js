44c528503a8959a2ea4bcff7bbefbafaddcd60e6
taiga-kasukabe <taiga.kasukabe@gmail.com>
2021-03-20 00:16:36 +0900

    main-scene.jsの大枠作ってみました

--- main-scene.js
+++ main-scene.js
@@ -1,13 +1,34 @@
 phina.define('MainScene', {
   superClass: 'CanvasScene',
-  init: function() {
+  init: function () {
     this.superInit()
     // 背景色を指定
     this.backgroundColor = '#444'
     // ラベルを生成
-    this.label = Label('Hello, phina.js!').addChildTo(this)
-    this.label.x = this.gridX.center() // x 座標
-    this.label.y = this.gridY.center() // y 座標
-    this.label.fill = 'white' // 塗りつぶし色
+    Label({
+      text: '媚びメモ', //テキスト内容
+      fontSize: 64, //文字サイズ
+      fill: 'pink', //文字塗りつぶし色
+      fontFamily: "'Consolas', 'Monaco', 'ＭＳ ゴシック'", //フォント
+    }).addChildTo(this).setPosition(320, 400);
+
+    //ボタンの設定
+    var button = Button({
+      x: 320,             // x座標
+      y: 600,             // y座標
+      width: 150,         // 横サイズ
+      height: 100,        // 縦サイズ
+      text: "スタート",     // 表示文字
+      fontSize: 32,       // 文字サイズ
+      fontColor: 'white', // 文字色
+      cornerRadius: 10,   // 角丸み
+      fill: 'skyblue',    // ボタン色
+      stroke: 'pink',     // 枠色
+      strokeWidth: 5,     // 枠太さ
+    }).addChildTo(this);
+
+    //ボタンが押された時の処理
+    button.onpointend = function () {
+    };
   }
 })
