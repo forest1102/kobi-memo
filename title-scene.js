e27a10d4856614b7167be0a61d8654a59c0e0a60
taiga-kasukabe <taiga.kasukabe@gmail.com>
2021-03-20 15:44:09 +0900

    edit Title Scene with error

--- title-scene.js
+++ title-scene.js
@@ -3,31 +3,34 @@ phina.define('TitleScene', {
   init(option) {
     this.superInit(option)
     // 背景色を指定
-    this.backgroundColor = '#444'
-    // ラベルを生成
+    this.backgroundColor = '#006D77'
+    // スプライト画像作成
+    // Sprite('tomapiko')
+    // .addChildTo(this)
+    // .setPosition(this.gridX.center(), this.gridY.center())
+    // // ラベルを生成
     Label({
-      text: '媚びメモ', //テキスト内容
+      text: 'こびメモ', //テキスト内容
       fontSize: 64, //文字サイズ
-      fill: 'pink', //文字塗りつぶし色
-      fontFamily: "'Consolas', 'Monaco', 'ＭＳ ゴシック'" //フォント
+      fill: '#e29578', //文字塗りつぶし色
+      fontFamily: "'Consolas', 'Monaco', 'あさご本丸ゴシックmini'" //フォント
     })
       .addChildTo(this)
       .setPosition(this.gridX.center(), this.gridY.center())
 
     //ボタンの設定
     var button = Button({
-      x: 320, // x座標
-      y: 250, // y座標
-      width: 130, // 横サイズ
-      height: 80, // 縦サイズ
-      text: 'スタート', // 表示文字
-      fontSize: 32, // 文字サイズ
-      fontColor: 'white', // 文字色
+      width: 200, // 横サイズ
+      height: 40, // 縦サイズ
+      text: '\t\t\tこびをうる', // 表示文字
+      fontSize: 24, // 文字サイズ
+      fontColor: 'black', // 文字色
       cornerRadius: 10, // 角丸み
-      fill: 'skyblue', // ボタン色
-      stroke: 'pink', // 枠色
+      fill: '#FFDDD2', // ボタン色
+      stroke: 'black', // 枠色
       strokeWidth: 5 // 枠太さ
-    }).addChildTo(this)
+    }).addChildTo(this).setPosition(this.gridX.center(), this.gridY.center()*1.7)
+
     //ボタンが押された時の処理
     button.onpointend = () => {
       this.exit()
