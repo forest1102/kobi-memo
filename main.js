1a8257e3ef5fff62f199ef0ae5863b60721988a9
taiga-kasukabe <taiga.kasukabe@gmail.com>
2021-03-20 17:55:45 +0900

    影付き文字を追加

--- main.js
+++ main.js
@@ -1,26 +1,34 @@
 // phina.js をグローバル領域に展開
 phina.globalize()
+
 //サイズ指定用の定数
 var SCREEN_X = 640
 var SCREEN_Y = 360
+
+// var ASSETS = {
+//   image: {
+//     'tomapiko': 'https://rawgit.com/phinajs/phina.js/develop/assets/images/tomapiko.png'
+//   },
+// }
+
 // メイン処理
-phina.main(function() {
+phina.main(function () {
   // アプリケーション生成
   var app = GameApp({
     startLabel: 'title',
     //screenのサイズ変更
     width: SCREEN_X,
     height: SCREEN_Y,
-    // assets: {
-    //   image: {
-    //     tomapiko:
-    //       'https://rawgit.com/phinajs/phina.js/develop/assets/images/tomapiko.png'
-    //   }
-    // },
     scenes: [
       {
         className: 'TitleScene',
         label: 'title',
+        assets: {
+          image: {
+            tomapiko:
+              'https://rawgit.com/phinajs/phina.js/develop/assets/images/tomapiko.png'
+          }
+        },
         nextLabel: 'login'
       },
       {
--- title-scene.js
+++ title-scene.js
@@ -3,23 +3,59 @@ phina.define('TitleScene', {
   init(option) {
     this.superInit(option)
     // 背景色を指定
-    this.backgroundColor = '#006D77'
+    this.backgroundColor = '#EDF6F9'
     // スプライト画像作成
     // Sprite('tomapiko')
-    // .addChildTo(this)
-    // .setPosition(this.gridX.center(), this.gridY.center())
-    // // ラベルを生成
+    //   .addChildTo(this)
+    //   .setPosition(this.gridX.center(), this.gridY.center())
+    // ラベルを生成
     Label({
-      text: 'こびメモ', //テキスト内容
-      fontSize: 64, //文字サイズ
-      fill: '#e29578', //文字塗りつぶし色
-      fontFamily: "'Consolas', 'Monaco', 'あさご本丸ゴシックmini'" //フォント
+      text: 'こびうり', //テキスト内容
+      fontSize: 50, //文字サイズ
+      fill: '#E29578', //文字塗りつぶし色
+      fontFamily: "'Consolas', 'Monaco', 'あさご本丸ゴシックmini'", //フォント
     })
       .addChildTo(this)
-      .setPosition(this.gridX.center(), this.gridY.center())
+      .setPosition(this.gridX.center()*0.39, this.gridY.center()*0.39)
+      .setRotation(340)
+
+
+    Label({
+      text: 'こびうり', //テキスト内容
+      fontSize: 50, //文字サイズ
+      fill: '#FFDDD2', //文字塗りつぶし色
+      fontFamily: "'Consolas', 'Monaco', 'あさご本丸ゴシックmini'", //フォント
+    })
+      .addChildTo(this)
+      .setPosition(this.gridX.center()*0.4, this.gridY.center()*0.4)
+      .setRotation(340)
+
+
+      Label({
+        text: 'メモリアル', //テキスト内容
+        fontSize: 50, //文字サイズ
+        fill: '#E29578', //文字塗りつぶし色
+        fontFamily: "'Consolas', 'Monaco', 'あさご本丸ゴシックmini'", //フォント  
+      })
+        .addChildTo(this)
+        .setPosition(this.gridX.center()*0.49, this.gridY.center()*0.59)
+        .setRotation(340)
+  
+
+    Label({
+      text: 'メモリアル', //テキスト内容
+      fontSize: 50, //文字サイズ
+      fill: '#FFDDD2', //文字塗りつぶし色
+      fontFamily: "'Consolas', 'Monaco', 'あさご本丸ゴシックmini'", //フォント
+    })
+      .addChildTo(this)
+      .setPosition(this.gridX.center()*0.5, this.gridY.center()*0.6)
+      .setRotation(340)
+
+
 
     //ボタンの設定
-    var button = Button({
+    let button = Button({
       width: 200, // 横サイズ
       height: 40, // 縦サイズ
       text: '\t\t\tこびをうる', // 表示文字
@@ -29,11 +65,23 @@ phina.define('TitleScene', {
       fill: '#FFDDD2', // ボタン色
       stroke: 'black', // 枠色
       strokeWidth: 5 // 枠太さ
-    }).addChildTo(this).setPosition(this.gridX.center(), this.gridY.center()*1.7)
+    }).addChildTo(this).setPosition(this.gridX.center(), this.gridY.center() * 1.7)
+
+    Label({
+      text: '▶️', //テキスト内容
+      fontSize: 25, //文字サイズ
+      fill: '#e29578', //文字塗りつぶし色
+      fontFamily: "'Consolas', 'Monaco', ''", //フォント
+      stroke: 'black',
+      strokeWidth: 4
+    }).addChildTo(this).setPosition(this.gridX.center()*0.8, this.gridY.center() * 1.71)
+
+
 
     //ボタンが押された時の処理
     button.onpointend = () => {
       this.exit()
     }
+
   }
 })
