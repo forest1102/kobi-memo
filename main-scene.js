afbcf4b2b35fd02c5aa3937ec8dd2733cf05651d
iKerr3721 <iKerr3721@gmail.com>
2021-03-20 22:55:43 +0900

    background of main

--- main-scene.js
+++ main-scene.js
@@ -14,13 +14,34 @@ phina.define('MainScene', {
     fadeIn.attachTo(sprite)
     sprite.tweener.wait(3000)
     sprite.tweener.scaleTo(5.0, 3000).play()
-
     // for(person of option.people){
     //   Sprite(person.image)
     //     .addChildTo(this)
     //     .setPosition(this.gridX.center(), this.gridY.center())
     // }
-  }
-})
+
+    // 背景色を指定
+    this.backgroundColor = '#EDF6F9'
+    // 固定飾り女性
+    Sprite('woman')
+      .addChildTo(this)
+      .setPosition(this.gridX.center() * 1.8, this.gridY.center() * 1.2)
+      .setScale(0.16, 0.16)
+    Sprite('man')
+      .addChildTo(this)
+      .setPosition(this.gridX.span(2), this.gridY.span(3))
+      .setScale(0.16, 0.16)
+      .setRotation(325)
+    // Sprite('hukidashi')
+    //   .addChildTo(this)
+    //   .setPosition(this.gridX.center() * 1.5, this.gridY.span(2))
+    //   .setScale(0.15, 0.15)
+
+  },
+
+
+
+}
+)
 
 
